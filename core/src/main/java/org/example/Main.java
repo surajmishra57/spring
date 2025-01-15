2240fc2fb8f69fc77003af69ef7b383adbba9869
Suraj Mishra <suraj.mishra@iot83.com>
2025-01-15 10:11:41 +0530

    spring core module added with spring dependency

--- core/src/main/java/org/example/Main.java
+++ core/src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        System.out.println(applicationContext);
     }
 }
